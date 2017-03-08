94840a8aed2553d5ec76c44538a1083cbae8e84b
Alexandru Balan <alexandru.balan0505@gmail.com>
2017-03-08 14:47:06 +0200

    Small changes on LiveObjects services

--- app/liveObjectsRequests.js
+++ app/liveObjectsRequests.js
@@ -3,15 +3,29 @@ exports.getData = function (options, res) {
     http.get(options, function (response) {
         var stream = require('stream');
         var out = new stream.Readable();
+        var outJSON = [];
         var full = "";
         response.on('data', function (data) {
             full += data;
         });
 
         response.on('end', function () {
-            out.push(full);
+            var data = JSON.parse(full);
+            filterReq(data, outJSON);
+            out.push(JSON.stringify(outJSON, null, 4));
             out.push(null);
             out.pipe(res);
         });
     });
 };
+
+function filterReq(data, out) {
+    data.forEach(function (d) {
+       var temp = {
+           streamId: d.streamId,
+           location: d.location,
+           value: d.value
+       };
+       out.push(temp);
+    });
+}
