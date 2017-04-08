04f10592b8b459b098fa68057a6be43541ceef62
Alexandru Balan <alexandru.balan0505@gmail.com>
2017-04-09 00:59:09 +0300

    fix web bugs

--- web/app/modules/auth/auth.js
+++ web/app/modules/auth/auth.js
@@ -10,12 +10,12 @@ angular
 				.state('login', {
 					url: '/login',
 					templateUrl: '/templates/login.html',
-					controller: 'SecurityController as vm'
+					// controller: 'SecurityController as vm'
 				})
 				.state('register', {
 						url: '/register',
 						templateUrl: '/templates/register.html',
-						controller: 'SecurityController as vm'
+						// controller: 'SecurityController as vm'
 					});
 
 		}])
