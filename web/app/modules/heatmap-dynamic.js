b788688be42efebab93981e00c15ae4b9b898afc
alin1popa <alin1popa@gmail.com>
2017-04-18 21:42:05 +0300

    Google Maps embed

--- web/app/modules/heatmap-dynamic.js
+++ web/app/modules/heatmap-dynamic.js
@@ -2,10 +2,11 @@
 var heatmap;
 
 function initGoogleMaps(){
+
 }
 
 setTimeout(function(){
-    //do this after view has loaded :)
+    /*
     console.log('success!');
 
      heatmap = h337.create({
@@ -16,4 +17,19 @@ setTimeout(function(){
         max: 5,
         data: [{ x: 10, y: 15, value: 5}]
     });
+*/
+    map = new google.maps.Map(document.getElementById('map-container'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+
+    heatmap = h337.create({
+        container: document.getElementById('map-container')
+    });
+
+    heatmap.setData({
+        max: 5,
+        data: [{ x: 10, y: 15, value: 5}]
+    });
+
 }, 5000);
