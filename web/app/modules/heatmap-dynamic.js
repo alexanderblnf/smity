df710773fb28547d55bc057528ee3dcff6b6cc8a
Alin <alin1popa@gmail.com>
2017-04-20 11:45:19 +0300

    Config object for heatmap

--- web/app/modules/heatmap-dynamic.js
+++ web/app/modules/heatmap-dynamic.js
@@ -3,39 +3,40 @@ function googleMapsLoaded(){
 
 }
 
-function initGoogleMaps(lat, lng, zoomlvl, containerid){
-    var coord = new google.maps.LatLng(lat, lng);
+function initGoogleMaps(mapconfig){
+    var coord = new google.maps.LatLng(mapconfig.centerlat, mapconfig.centerlng);
     var options = {
-        zoom: zoomlvl,
+        zoom: mapconfig.centerzoom,
         center: coord
     };
-    map = new google.maps.Map(document.getElementById(containerid), options);
+    map = new google.maps.Map(document.getElementById(mapconfig.containername), options);
     return map;
 }
 
-function initHeatmap(map, minopacity, maxopacity, bgred, bggreen, bgblue, bgalpha, radius, valfield){
+function initHeatmap(map, mapconfig){
+
     var heatmap = new HeatmapOverlay(map,
         {
-            "minOpacity": minopacity,
-            "maxOpacity": maxopacity,
-            "backgroundColor" : "rgba("+bgred+","+bggreen+","+bgblue+","+bgalpha+")",
-            "radius": radius,
+            "minOpacity": mapconfig.minopacity,
+            "maxOpacity": mapconfig.maxopacity,
+            "backgroundColor" : "rgba("+mapconfig.bgred+","+mapconfig.bggreen+","+mapconfig.bgblue+","+mapconfig.bgalpha+")",
+            "radius": mapconfig.radius,
             "scaleRadius": true,
             "useLocalExtrema": false,
-            latField: 'lat',
-            lngField: 'long',
-            valueField: valfield
+            latField: mapconfig.latfield,
+            lngField: mapconfig.lngfield,
+            valueField: mapconfig.valfield
         }
     );
 
     return heatmap;
 }
 
-function heatmapPlotData(heatmap, fromtime, totime, space, relative, minval, maxval, centerlat, centerlng){
+function heatmapPlotData(heatmap, mapconfig, fromtime, totime){
     $.get({ url: "http://localhost:8080/elastic/heatmapdata/temperature/"+fromtime+"/"+totime,
             success: function( data ) {
                 //var cells = {};
-                if (relative === true) {
+                if (mapconfig.extremarelative === true) {
                     minval = Number.MAX_VALUE;
                     maxval = Number.MIN_VALUE;
                     $.each(data, function (index, item) {
@@ -56,6 +57,10 @@ function heatmapPlotData(heatmap, fromtime, totime, space, relative, minval, max
                         cells[latindex][lngindex].push = index;*/
                     });
                 }
+                else{
+                    minval = mapconfig.minval;
+                    maxval = mapconfig.maxval;
+                }
                 var testData = {
                     min: minval,
                     max: maxval,
@@ -72,12 +77,42 @@ setTimeout(function(){
     var CITY_LAT = 46.0684893;
     var CITY_LNG = 23.5634674;
 
-    var map = initGoogleMaps(CITY_LAT, CITY_LNG, 15, 'map-container');
-    var heatmap = initHeatmap(map, 0.1, 0.8, 255, 0, 0, 0.0, 0.0008, 'temperature');
+    var mapconfig = {};
+    //coordinates of map center
+    mapconfig.centerlat = CITY_LAT;
+    mapconfig.centerlng = CITY_LNG;
+    //zoom level on google maps
+    mapconfig.centerzoom = 15;
+    //name of div where google maps is drawn
+    mapconfig.containername = 'map-container';
+    //opacity of heatmap extremas
+    mapconfig.minopacity = 0.1;
+    mapconfig.maxopacity = 0.8;
+    //heatmap background color
+    mapconfig.bgred = 255;
+    mapconfig.bggreen = 0;
+    mapconfig.bgblue = 0;
+    mapconfig.bgalpha = 0.0;
+    //coord radius of heatmap point
+    mapconfig.radius = 0.0008;
+    //name of fields in data
+    mapconfig.valfield = 'temperature';
+    mapconfig.latfield = 'lat';
+    mapconfig.lngfield = 'long';
+    //use extremas from data as min/max for heatmap
+    mapconfig.extremarelative = true;
+    //if extremarelative=false
+    //use the following values as heatmap scale min/max
+    mapconfig.minval = null;
+    mapconfig.maxval = null;
+
+
+    var map = initGoogleMaps(mapconfig);
+    var heatmap = initHeatmap(map, mapconfig);
 
     var fromtime = 1480687145;
     var totime = 1492551145;
 
-    heatmapPlotData(heatmap, fromtime, totime, 0.0008, true, null, null, CITY_LAT, CITY_LNG);
+    heatmapPlotData(heatmap, mapconfig, fromtime, totime);
 
 }, 3000);
